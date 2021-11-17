de91c7f5a6f19cc454b0dc04343dcd8ff34e1904
Derek Reid <n00192978@student.iadt.ie>
2021-11-17 18:37:21 +0000

    End of class commit

--- src/pages/projects/AllProjects.vue
+++ src/pages/projects/AllProjects.vue
@@ -1,21 +1,39 @@
 <template>
-  <div>
-    This is the AllProjects Component  
-    <div class="row">
-        <div class="col-md-4">
-            <b-card
-            header="Projects"
-            header-text-variant="white"
-            header-bg-variant="dark"    
-            v-for="project in projects"
+  <div class="columns">
+    <div class="column">
+        <b-field>
+            <b-input v-model="searchTerm"></b-input>
+        </b-field>
+        <div class="card"
+            v-for="project in filteredprojects"
             :key="project.id"
-            >
-            Title: {{ project.title }}<br>
-            Decsription: {{ project.description }}<br> 
-            <router-link :to="{ name: project.demo }">Demo</router-link>
-            </b-card>
-            <br>
-            <br>
+        >
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholder/1280x960.png" alt="Placeholder image">                        
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-content">
+                            <p class="title is-4"> {{ project.title }} </p>
+                            <p class="subtitle is-6">
+                                <b-taglist>
+                                    <b-tag
+                                    v-for="tag in project.tags"
+                                    :key="tag"
+                                    type="is-primary"
+                                    >
+                                    </b-tag>
+                                </b-taglist>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="content">
+                        {{ project.description }}
+                    </div>
+                </div>
+            <router-link v-if="project.demo" :to="{ name: project.demo }">Demo</router-link>
         </div> 
     </div>
   </div>
@@ -29,7 +47,17 @@ export default {
   },
   data(){
       return{
-          projects:[]
+          projects:[],
+          //work on 17-11-2021
+          searchTerm: ""
+      }
+  },
+  //work on 17-11-2021 searchTerm works here
+  computed:{
+      filteredprojects: function () {
+          return this.projects.filter(project => {
+              return project.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+          })
       }
   },
   mounted(){
@@ -43,7 +71,10 @@ export default {
               console.log(data)
               this.projects = data
           })
-      }
+      },
+    //   searchProjects(){
+
+    //   }
   }
 }
 </script>
